99e9017744ca2dbce5d6eeb14669bd93dd9049c9
Mikel Ward <mikel@mikelward.com>
2011-05-04 13:56:30 +1000

    Simplify code a bit, add some more comments.
    The main simplification is to check for problems and exit,
    so we only need an if branch, rather than having nested
    if/else branches to follow the successful code path.

--- root.c
+++ root.c
@@ -2,6 +2,15 @@
  * root
  *
  * simple reimplementation of sudo
+ *
+ * Mikel Ward <mikel@mikelward.com>
+ */
+
+/*
+ * terminology:
+ *  - absolute path     path to a file starting with /
+ *  - qualified path    path to a file containing a /
+ *  - unqualified path  path to a file not containing a /
  */
 
 #define _BSD_SOURCE             /* strdup(), initgroups(), etc. */
@@ -28,8 +37,6 @@ void usage(void)
 
 int main(int argc, char **argv)
 {
-    int is_allowed;
-
     if (argc < 2) {
         usage();
         exit(ROOT_INVALID_USAGE);
@@ -37,85 +44,7 @@ int main(int argc, char **argv)
 
     initlog(PROGNAME);
 
-    is_allowed = in_group(ROOT_GID);
-    if (is_allowed) {
-        int status;
-        const char *command = argv[1];
-        char *const *newargv = argv+1;
-        char *command_path = NULL;
-
-        debug("Command to run is %s\n", command);
-
-        if (is_unqualified_path(command)) {
-            /*debug("Unqualified path, will search PATH");*/
-            const char *pathenv = getenv("PATH");
-            if (pathenv == NULL) {
-                error("Cannot get PATH environment variable");
-                exit(ROOT_SYSTEM_ERROR);
-            }
-            debug("PATH=%s", pathenv);
-            command_path = get_command_path(command, pathenv);
-            if (command_path == NULL) {
-                error("Cannot find %s in PATH", command);
-                exit(ROOT_COMMAND_NOT_FOUND);
-            }
-            else if (command_path[0] != DIRSEP) {
-                /*
-                 * don't allow running a command in the current directory
-                 * or any command with a relative path
-                 * (unless user specified a qualified path, in which case
-                 *  we don't enter this function anyway)
-                 */
-                char *resolved_path = realpath(command_path, NULL);
-                if (resolved_path != NULL) {
-                    error("You tried to run %s, but this would run %s", command, resolved_path);
-                }
-                else {
-                    error("You tried to run %s, which is a relative path", command, resolved_path);
-                }
-                error("Running commands via \"\" or \".\" in PATH is prohibited for security reasons");
-                error("Run man 1 root for the reasons and solutions");
-                exit(ROOT_RELATIVE_PATH_DISALLOWED);
-            }
-        }
-        else {
-            /*debug("Qualified path, bypassing PATH");*/
-            command_path = strdup(command);
-        }
-
-        /*
-         * We have decided what to run,
-         * log the fact before we actually do anything
-         *
-         * Currently we do this before calling setuid(),
-         * because the log message includes the username,
-         * and we want to log the calling username, not root
-         */
-        info("Running %s", command_path);
-
-        setup_groups(ROOT_UID);
-
-        if (!become_user(ROOT_UID)) {
-            error("Cannot become root");
-            /* system error because if this program is installed setuid,
-             * then become_user should always succeed */
-            exit(ROOT_SYSTEM_ERROR);
-        }
-
-        /*
-         * IMPORTANT
-         * This must stay as execv, never execvp.
-         */
-        status = execv(command_path, newargv);
-        if (status == -1) {
-            error("Cannot exec '%s': %s", command_path, strerror(errno));
-            exit(ROOT_ERROR_EXECUTING_COMMAND);
-        }
-        else {
-            /* execv does not return on success */
-        }
-    }
-    else {
+    if (!in_group(ROOT_GID)) {
         const char *groupname = get_group_name(ROOT_GID);
         if (groupname != NULL) {
             error("You must be in the %s group to run root", groupname);
@@ -125,6 +54,105 @@ int main(int argc, char **argv)
         }
         exit(ROOT_PERMISSION_DENIED);
     }
+
+    /* skip over our own program name */
+    argv++, argc--;
+
+    /*
+     * first argument is the command to run,
+     * which we will do some checks on first;
+     * all arguments (including the first)
+     * will be passed verbatim to exec as argv
+     */
+    const char *command = argv[0];
+    char *const *newargv = argv;
+    char *command_path = NULL;
+
+    debug("Command to run is %s\n", command);
+
+    /*
+     * Allowed:
+     * - root /path/to/command
+     * - root ./command
+     *
+     * Allowed only if the first matching entry in PATH starts with a "/":
+     * - root command
+     */
+    if (is_unqualified_path(command)) {
+        /*debug("Unqualified path, will search PATH");*/
+        const char *pathenv = getenv("PATH");
+        if (pathenv == NULL) {
+            error("Cannot get PATH environment variable");
+            exit(ROOT_SYSTEM_ERROR);
+        }
+        debug("PATH=%s", pathenv);
+        command_path = get_command_path(command, pathenv);
+        if (command_path == NULL) {
+            error("Cannot find %s in PATH", command);
+            exit(ROOT_COMMAND_NOT_FOUND);
+        }
+        else if (command_path[0] != DIRSEP) {
+            /*
+             * don't allow running a command in the current directory
+             * or any command with a relative path
+             * (unless user specified a qualified path, in which case
+             *  we don't enter this function anyway)
+             */
+            char *resolved_path = realpath(command_path, NULL);
+            if (resolved_path != NULL) {
+                error("You tried to run %s, but this would run %s", command, resolved_path);
+            }
+            else {
+                error("You tried to run %s, which is a relative path", command, resolved_path);
+            }
+            error("Running commands via \"\" or \".\" in PATH is prohibited for security reasons");
+            error("Run man 1 root for the reasons and solutions");
+            exit(ROOT_RELATIVE_PATH_DISALLOWED);
+        }
+    }
+    else {
+        /*debug("Qualified path, bypassing PATH");*/
+        command_path = strdup(command);
+    }
+
+    /*
+     * TODO
+     * consider converting command_path into an absolute path
+     * so that any log message mentioning the command is unambiguous
+     */
+
+    /*
+     * We have decided what to run,
+     * log the fact before we actually do anything
+     *
+     * Currently we do this before calling setuid(),
+     * because the log message includes the username,
+     * and we want to log the calling username, not root
+     */
+    info("Running %s", command_path);
+
+    /*
+     * XXX
+     * should setup_groups be part of become_user() ?
+     */
+    setup_groups(ROOT_UID);
+
+    if (!become_user(ROOT_UID)) {
+        error("Cannot become root");
+        /* system error because if this program is installed setuid,
+            * then become_user should always succeed */
+        exit(ROOT_SYSTEM_ERROR);
+    }
+
+    /*
+     * IMPORTANT
+     * This must stay as execv, never execvp.
+     */
+    if (execv(command_path, newargv) == -1) {
+        error("Cannot exec '%s': %s", command_path, strerror(errno));
+        exit(ROOT_ERROR_EXECUTING_COMMAND);
+    }
+    /* execv does not return on success */
 }
 
 /* vim: set ts=4 sw=4 tw=0 et:*/
